956d93426d80ab1b2a990acccb2ef2940df1f9fe
KAK <kak@gmail.com>
2016-06-26 17:49:25 -0400

    creation

---
+++ MeMe V-1.0/Meme.swift
@@ -0,0 +1,19 @@
+//
+//  Meme.swift
+//  MeMe V-1.0
+//
+//  Created by Vasanth Kodeboyina on 6/12/16.
+//  Copyright © 2016 Anish Kodeboyina. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct MemeModel
+{
+    var topText: String!
+    var bottomText: String!
+    var originalImage: UIImage!
+    var memedImage: UIImage!
+}
+    
